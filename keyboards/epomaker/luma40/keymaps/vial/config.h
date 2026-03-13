332eba32585ca381814156747efcfd55c94b2d3a
shyguy80 <30806692+shyguy80@users.noreply.github.com>
2026-03-13 16:27:41 +0100

    Add files via upload
    Initial commit

---
+++ keyboards/epomaker/luma40/keymaps/vial/config.h
@@ -0,0 +1,35 @@
+/* Copyright 2025 Carlos Eduardo de Paula <carlosedp@gmail.com>
+ * Copyright 2025 EPOMAKER <https://github.com/Epomaker>
+ * Copyright 2023 LiWenLiu <https://github.com/LiuLiuQMK>
+ * Copyright 2021 QMK <https://github.com/qmk/qmk_firmware>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+// Custom configuration
+
+// Vial configuration
+#define VIAL_KEYBOARD_UID {0xA3, 0xF2, 0xC1, 0xB4, 0xE5, 0xD6, 0x09, 0x87}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 11}
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
+
+#define VIAL_TAP_DANCE_ENTRIES 10
+
+#define VIAL_COMBO_ENTRIES 10
+
+#define VIAL_KEY_OVERRIDE_ENTRIES 6
+
+#define VIAL_ALT_REPEAT_KEY_ENTRIES 6
